1a7010249c6aec4f6b00dc18506752412c767913
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-04-05 13:10:49 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '37c58aaa9470b01f577f1f7974118b53'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '01477c53-6a45-4b7a-ae89-eccc019df35e'
+};
